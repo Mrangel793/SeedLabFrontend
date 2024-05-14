833f737f0547081893a9b9c57ff2007025f6ac5a
Marly Rangel <rangelmarly@gmail.com>
2024-05-13 22:24:07 -0500

    actualizacion de ruta

--- src/app/servicios/emprendedor.service.ts
+++ src/app/servicios/emprendedor.service.ts
@@ -8,9 +8,9 @@ import {Observable} from 'rxjs';
 })
 export class EmprendedorService {
 
-  url = 'http://127.0.0.1:8000/emprendedor/'
+  url = 'http://127.0.0.1:8000/api/emprendedor/'
 
-  constructor(private http: HttpClient) { }
+  constructor(private http:HttpClient) { }
 
     getEmpresas(documento: string): Observable<any>{
       return this.http.get(this.url+documento);
